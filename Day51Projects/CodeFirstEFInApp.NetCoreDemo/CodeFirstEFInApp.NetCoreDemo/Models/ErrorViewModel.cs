78a84a37f826a6ef3a22ff0cbc183d1d13f3a81c
akshitsaini2003 <akshitsaini2003@gmail.com>
2026-03-19 15:35:58 +0530

    Day51Projects 19March2026

---
+++ Day51Projects/CodeFirstEFInApp.NetCoreDemo/CodeFirstEFInApp.NetCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CodeFirstEFInApp.NetCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Day51Projects/CodeFirstEFInApp.NetCoreDemo/CodeFirstEFInApp.NetCoreDemo/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstEFInApp.NetCoreDemo.Models
+{
+    public class Product
+    {
+        public int ProductID {  get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+        [Display(Name ="Who Buyed")]
+        [ForeignKey("Customer")]
+        public int CustomerID {  get; set; }
+        public Customer Customer { get; set; }
+    }
+}
