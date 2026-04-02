fd91d52bd4bd1c39a50145c08dde1a91a0159ea9
akshitsaini2003 <akshitsaini2003@gmail.com>
2026-04-02 15:00:16 +0530

    Week1

--- Day58Projects(SaturdayTest)/ProductCRUD/ProductCRUD/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
+++ Day58Projects(SaturdayTest)/ProductCRUD/ProductCRUD/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d32001bc20468612d3055a71905bb0bd0c8f8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403f71fd6c74dd6a5a7abe06f02bed1f5cd9134e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day61Projects/WebApiInAsp.netcoreMvcDemo/WebApiInAsp.netcoreMvcDemo/Controllers/AuthenticationUIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiInAsp.netcoreMvcDemo.Controllers
+{
+    public class AuthenticationUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
